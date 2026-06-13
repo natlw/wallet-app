1ece6375f4f3dee4cd91d3fc96c972feaceae548
natlw <natlewa021@gmail.com>
2026-06-13 18:24:40 +0200

    feat: register Asset, WalletTransaction and AssetCache models in Django admin panel

--- wallet/admin.py
+++ wallet/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Asset, WalletTransaction, AssetCache
 
-# Register your models here.
+
+@admin.register(Asset)
+class AssetAdmin(admin.ModelAdmin):
+    # kolumny
+    list_display = ("name", "symbol", "asset_type", "api_id")
+    # filtry
+    list_filter = ("asset_type",)
+    # wyszukiwanie
+    search_fields = ("name", "symbol")
+
+
+@admin.register(WalletTransaction)
+class WalletTransactionAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "asset",
+        "transaction_type",
+        "quantity",
+        "price_per_unit",
+        "date",
+    )
+    list_filter = ("transaction_type", "date", "asset")
+    search_fields = ("user__username", "asset__name")
+
+
+@admin.register(AssetCache)
+class AssetCacheAdmin(admin.ModelAdmin):
+    list_display = ("asset", "price_pln", "last_updated")
