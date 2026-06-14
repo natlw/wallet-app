acbd5c15711fa2953e2cd6d47a3aa9020b48a552
natlw <natlewa021@gmail.com>
2026-06-14 15:55:49 +0200

    feat: prepare wallet app for Render deployment

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "dev-key-fallback")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".onrender.com"]
 
 
 # Application definition
